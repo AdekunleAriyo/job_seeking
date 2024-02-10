49b8f968168f38d4205d9be58c640dec33866ae6
AdekunleAriyo <dekunlearry@gmail.com>
2024-02-10 23:47:35 +0100

    Utility card for homepage

--- lib/utils/recent_job_card.dart
+++ lib/utils/recent_job_card.dart
@@ -15,22 +15,60 @@ class RecentJobCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: Colors.grey),
-      child: Row(
-        children: [
-          Container(
-            height: 40,
-            child: Image.asset(logoImagePath),
-          ),
-          Column(
-            children: [
-              Text(jobTitle),
-              Text(companyName),
-            ],
-          ),
-          Text(hourlyRate.toString())
-        ],
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12.0),
+      child: Container(
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+            color: Colors.grey[200],
+            border: Border.all(color: Colors.white),
+            borderRadius: BorderRadius.circular(8)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Container(
+                    height: 40,
+                    padding: EdgeInsets.all(5),
+                    color: Colors.grey[300],
+                    child: Image.asset(logoImagePath),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      jobTitle,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      companyName,
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: Container(
+                  color: Colors.green[300],
+                  padding: EdgeInsets.all(5),
+                  child: Text(
+                    '\$' + hourlyRate.toString() + '/h',
+                    style: TextStyle(color: Colors.white),
+                  )),
+            )
+          ],
+        ),
       ),
     );
   }
